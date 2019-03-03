5ebea7b001d8a4ac56401e8ff8c800d657f69e4f
kosiginiv83 <kosiginiv83@ADMIN-LAPTOP>
2019-03-03 13:40:25 +0800

    no message

--- request-handling/file_server/app/urls.py
+++ request-handling/file_server/app/urls.py
@@ -1,11 +1,7 @@
 from django.urls import path
 from app.views import FileList, file_content
 
-# Определите и зарегистрируйте конвертер для определения даты в урлах и наоборот урла по датам
-
-
 urlpatterns = [
-    # Определите схему урлов с привязкой к отображениям .views.FileList и .views.file_content
     path('', FileList.as_view(), name = 'file_list'),
     path('<int:year>-<int:month>-<int:day>', FileList.as_view(), name='file_list'),
     path('file/<str:file>', file_content, name='file_content'),
--- request-handling/file_server/app/views.py
+++ request-handling/file_server/app/views.py
@@ -1,17 +1,17 @@
 import datetime
+import sys, os
 
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-from .settings import FILES_PATH
-#import settings
+from app.settings import FILES_PATH
 from django.http import HttpResponse
+
 
 class FileList(TemplateView):
     template_name = 'index.html'
 
     def get_context_data(self, date=None):
-        # Реализуйте алгоритм подготавливающий контекстные данные для шаблона по примеру:
 
         return {
             'files': [
@@ -23,12 +23,14 @@ class FileList(TemplateView):
         }
 
 
-
-def file_content(request, name):
-    # Реализуйте алгоритм подготавливающий контекстные данные для шаблона по примеру:
-
+def file_content(request, file):
+    with open(os.path.join(FILES_PATH, file)) as f:
+        content = f.read()
     return render(
         request,
         'file_content.html',
-        context={'file_name': 'file_name_1.txt', 'file_content': 'File content!'}
+        context={'file_name': file, 'file_content': content}
     )
+
+# os.stat(os.path.join(FILES_PATH, file))
+# content = open(os.path.join(FILES_PATH, file)).read()
