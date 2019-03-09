fb5036499d4d8679b0784f632884c9eb3660d723
kosiginiv83 <kosiginiv83@ADMIN-LAPTOP>
2019-03-09 10:32:05 +0800

    no message

--- dynamic-templates/task1/app/views.py
+++ dynamic-templates/task1/app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 import csv
+import os.path
 from app.settings import BASE_DIR
 
 
@@ -10,11 +11,21 @@ class InflationView(TemplateView):
 
     def get(self, request, *args, **kwargs):
         base = os.path.join(BASE_DIR, 'inflation_russia.csv')
-        with open(base) as csv:
-            reader = tuple(csv.DictReader(csv))[:]
+        field_names = ('Year', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', \
+                        'Aug', 'Sep', 'Oct', 'Nov', 'Des', 'Summ')
+        th_names = ('Год', 'Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', \
+                    'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь', 'Всего')
+
+        with open(base, encoding='UTF8', newline='') as file:
+            file.readline()
+            reader = tuple(csv.DictReader(file, fieldnames=field_names, delimiter=';'))
 
         context = {
-            'base': reader
+            'base': reader,
+            'th_names': th_names,
+            'td_class': None
         }
         return render(request, self.template_name,
                       context)
+
+#<p style="font-size: 120%; font-family: monospace; color: #cd66cc">Пример текста</p>
