3fbcbe72464afe05a761aefb655cdb4739c73d13
kosiginiv83 <kosiginiv83@ADMIN-LAPTOP>
2019-03-14 09:12:35 +0800

    no message

--- dynamic-templates/task2/app/views.py
+++ dynamic-templates/task2/app/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+import os
+from app.settings import STATIC_ROOT
+
 
 class MainView(TemplateView):
     template_name = 'app/main.html'
 
+
 class HomeView(TemplateView):
     template_name = 'app/home.html'
+    def get(self, request, *args, **kwargs):
+        context = {
+            'img': os.path.join(STATIC_ROOT, 'steve.jpg'),
+            'path': os.path.join(STATIC_ROOT, 'steve.jpg')
+        }
+        return render(request, self.template_name, context)
 
 
 class AboutView(TemplateView):
